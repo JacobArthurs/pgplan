4889bf09bdb5d9aef0dd707826d1fc10a855d5d2
Jacob <Arthursjacob01@gmail.com>
2026-02-15 18:43:43 -0500

    Adjust regex

--- internal/analyzer/context.go
+++ internal/analyzer/context.go
@@ -56,17 +56,28 @@ func collectContext(node *plan.PlanNode, parent *plan.PlanNode, depth int, ctx *
 	}
 }
 
-var columnRefRe = regexp.MustCompile(`\b(\w+)\.(\w+)\b`)
+var (
+	stringLiteralRe = regexp.MustCompile(`'[^']*'`)
+	columnRefRe     = regexp.MustCompile(`\b(\w+)\.(\w+)\b`)
+	castColRe       = regexp.MustCompile(`\(([a-zA-Z_]\w*)\)::`)
+)
 
 func ExtractConditionColumns(cond string) []string {
 	if cond == "" {
 		return nil
 	}
-	matches := columnRefRe.FindAllStringSubmatch(cond, -1)
+	cleaned := stringLiteralRe.ReplaceAllString(cond, "")
 	seen := make(map[string]bool)
 	var cols []string
-	for _, m := range matches {
+	for _, m := range columnRefRe.FindAllStringSubmatch(cleaned, -1) {
 		col := m[2]
+		if !seen[col] {
+			seen[col] = true
+			cols = append(cols, col)
+		}
+	}
+	for _, m := range castColRe.FindAllStringSubmatch(cleaned, -1) {
+		col := m[1]
 		if !seen[col] {
 			seen[col] = true
 			cols = append(cols, col)
